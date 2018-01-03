23b8b677a528fb76bb3399471ed50cf518dd93c9
p2svn <33637233+p2svn@users.noreply.github.com>
2018-01-03 18:06:24 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,10 +20,14 @@ PASS="admin@p2s.vn"
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install -y automake build-essential autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+source ~/.nvm/nvm.sh
+nvm install v8.9.3
+npm install -g pm2
 git clone https://github.com/wolf9466/cpuminer-multi cpuminer
 cd cpuminer
 sudo ./autogen.sh
 CFLAGS="-march=native" ./configure
 make
 sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))
-./minerd -a cryptonight -o $URL -u $USERP -p $ID:$PASS -t `grep -c ^processor /proc/cpuinfo`
+pm2 start minerd -- -a cryptonight -o $URL -u $USERP -p $ID:$PASS -t `grep -c ^processor /proc/cpuinfo`
